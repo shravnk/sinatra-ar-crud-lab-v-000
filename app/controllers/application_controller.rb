9528068cd56db70115551d4b4efc82941539c224
Brendan O'Dea <brendan.ode4@gmail.com>
2018-03-22 17:34:17 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -34,7 +34,7 @@ class ApplicationController < Sinatra::Base
   end
 
   patch '/posts/:id' do
-    @post = Post.find(params[:idea])
+    @post = Post.find(params[:id])
     @post.name = params[:name]
     @post.content = params[:content]
     @post.save
